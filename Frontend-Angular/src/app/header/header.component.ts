502acc6016bdac75f617d693bfa2f660d111665e
RLNunes <38779@alunos.isel.ipl.pt>
2026-04-04 19:54:42 +0100

    :fire: chore: remove unused `HeaderComponent` and template from project

--- Frontend-Angular/src/app/header/header.component.ts
+++
@@ -1,8 +0,0 @@
-﻿import { Component } from '@angular/core';
-import { GlobalHeaderComponent } from '../core/layout/global-header/global-header.component';
-@Component({
-  selector: 'app-header',
-  imports: [GlobalHeaderComponent],
-  templateUrl: './header.component.html',
-})
-export class HeaderComponent {}
