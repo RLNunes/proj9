e260237370820a9090e9a3816e9e040f6cd835fd
RLNunes <38779@alunos.isel.ipl.pt>
2026-04-04 19:53:37 +0100

    :fire: chore: remove `ShellComponent` from app routes as it is no longer in use

--- Frontend-Angular/src/app/app.routes.ts
+++ Frontend-Angular/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { ShellComponent } from './core/layout/shell/shell.component';
 import { homeRoutes } from './features/home/home.routes';
 import { adminRoutes } from './features/admin/admin.routes';
 import { consultaRoutes } from './features/consulta/consulta.routes';
@@ -8,7 +7,6 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
     path: '',
-    component: ShellComponent,
     children: [...homeRoutes, ...adminRoutes, ...consultaRoutes],
   },
   { path: '**', redirectTo: 'home' },
