002b11bd044df57cd1990edaba859d2ca0fc1688
RLNunes <38779@alunos.isel.ipl.pt>
2026-04-15 00:10:29 +0100

    :building_construction: feat: add validation annotations to `AuthLoginRequestDto` to ensure input constraints

--- CircPeticionario-WebApp/src/main/java/pt/ual/auth/dto/AuthLoginRequestDto.java
+++ CircPeticionario-WebApp/src/main/java/pt/ual/auth/dto/AuthLoginRequestDto.java
@@ -1,7 +1,17 @@
 package pt.ual.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthLoginRequestDto {
+
+  @NotBlank
+  @Size(max = 255)
   private String username;
+
+  @NotBlank
   private String password;
 
   public String getUsername() {
@@ -20,4 +30,3 @@ public class AuthLoginRequestDto {
     this.password = password;
   }
 }
-
