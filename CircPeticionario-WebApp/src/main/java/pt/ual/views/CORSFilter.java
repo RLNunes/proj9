6d2e0c2d285327c7b7886d1d688810a5a1c936bd
RLNunes <38779@alunos.isel.ipl.pt>
2026-04-15 11:18:51 +0100

    :building_construction: feat: restrict CORS policy to allow only local development origins

--- CircPeticionario-WebApp/src/main/java/pt/ual/views/CORSFilter.java
+++ CircPeticionario-WebApp/src/main/java/pt/ual/views/CORSFilter.java
@@ -1,20 +1,37 @@
- package pt.ual.views;
+package pt.ual.views;
 
- import javax.ws.rs.container.ContainerRequestContext;
- import javax.ws.rs.container.ContainerResponseContext;
- import javax.ws.rs.container.ContainerResponseFilter;
- import javax.ws.rs.ext.Provider;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 
- @Provider
- public class CORSFilter
-   implements ContainerResponseFilter {
-   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-      String origin = requestContext.getHeaderString("Origin");
-      responseContext.getHeaders().add("Access-Control-Allow-Origin", origin != null ? origin : "*");
-      responseContext.getHeaders().add("Vary", "Origin");
-     responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-     responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-     responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-     responseContext.getHeaders().add("Access-Control-Expose-Headers", "Authorization");
-   }
- }
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    /**
+     * CORS configurado apenas para desenvolvimento local.
+     * <p>
+     * Em ambientes integrados por Nginx / reverse proxy, esta política deve ser
+     * revista ou movida para a camada de proxy, conforme os origins reais.
+     */
+    private static final Set<String> ALLOWED_ORIGINS = new HashSet<>(Arrays.asList(
+            "http://localhost:4200",
+            "http://localhost"
+    ));
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        String origin = requestContext.getHeaderString("Origin");
+
+        if (origin != null && ALLOWED_ORIGINS.contains(origin)) {
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
+            responseContext.getHeaders().putSingle("Vary", "Origin");
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+            responseContext.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+        }
+    }
+}
