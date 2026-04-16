1d624d8efd99723f80645b7a5fae9fa1048870ca
RLNunes <38779@alunos.isel.ipl.pt>
2026-04-16 21:03:56 +0100

    :building_construction: feat: redesign global header with PrimeNG toolbar, new navigation, and avatar integration

--- Frontend-Angular/src/app/core/layout/global-header/global-header.component.ts
+++ Frontend-Angular/src/app/core/layout/global-header/global-header.component.ts
@@ -2,10 +2,11 @@
 import { RouterLink } from '@angular/router';
 import { UserMenuComponent } from '../user-menu/user-menu.component';
 import { ToolbarModule } from 'primeng/toolbar';
+import { Avatar } from 'primeng/avatar';
 
 @Component({
   selector: 'app-global-header',
-  imports: [RouterLink, UserMenuComponent, ToolbarModule],
+  imports: [RouterLink, UserMenuComponent, ToolbarModule, Avatar],
   templateUrl: './global-header.component.html',
 })
 export class GlobalHeaderComponent {
