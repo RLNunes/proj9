d08ba838b4d0f050219a40f52a423dc67653d3d9
RLNunes <38779@alunos.isel.ipl.pt>
2026-04-16 20:47:38 +0100

    :building_construction: feat: update global header with new branding assets and integrate PrimeNG toolbar

--- Frontend-Angular/src/app/core/layout/global-header/global-header.component.ts
+++ Frontend-Angular/src/app/core/layout/global-header/global-header.component.ts
@@ -1,9 +1,11 @@
 ﻿import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UserMenuComponent } from '../user-menu/user-menu.component';
+import { ToolbarModule } from 'primeng/toolbar';
+
 @Component({
   selector: 'app-global-header',
-  imports: [RouterLink, UserMenuComponent],
+  imports: [RouterLink, UserMenuComponent, ToolbarModule],
   templateUrl: './global-header.component.html',
 })
 export class GlobalHeaderComponent {
