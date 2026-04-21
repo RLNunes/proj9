839ed4f83ad515b13ad6c4b92a300c78c5620cdb
RLNunes <38779@alunos.isel.ipl.pt>
2026-04-21 20:18:20 +0100

    :building_construction: feat: integrate login modal in user menu and update authentication flow in header state service

--- Frontend-Angular/src/app/core/layout/user-menu/user-menu.component.ts
+++ Frontend-Angular/src/app/core/layout/user-menu/user-menu.component.ts
@@ -1,9 +1,16 @@
-﻿import { Component } from '@angular/core';
+﻿import {Component, ViewChild} from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
+import {LoginModalComponent} from '../../auth/login-modal/login-modal.component';
 @Component({
   selector: 'app-user-menu',
-  imports: [AvatarModule],
+  imports: [AvatarModule, LoginModalComponent],
   templateUrl: './user-menu.component.html',
 })
 export class UserMenuComponent {
+  @ViewChild(LoginModalComponent) loginModal?: LoginModalComponent;
+
+  onAvatarClick(): void {
+    // se não autenticado
+    this.loginModal?.open();
+  }
 }
