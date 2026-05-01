29dda5b78b2c76ddceb777da127cea1cf2ce93e7
RLNunes <38779@alunos.isel.ipl.pt>
2026-05-01 23:36:14 +0100

    :building_construction: feat: add `DomainSearchbarComponent` with template, styles, and unit tests

---
+++ Frontend-Angular/src/app/core/layout/domain-searchbar/domain-searchbar.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+@Component({
+  selector: 'app-domain-searchbar',
+  templateUrl: './domain-searchbar.component.html',
+})
+export class DomainSearchbarComponent {
+}
