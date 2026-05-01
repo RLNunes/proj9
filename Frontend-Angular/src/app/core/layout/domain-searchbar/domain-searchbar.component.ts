3911bfd6b8ecc203549b68a5cc7ec272bf7ca5c5
RLNunes <38779@alunos.isel.ipl.pt>
2026-05-01 22:41:30 +0100

    :fire: chore: remove unused `AdminLayoutComponent`, `ConsultaLayoutComponent`, and related components, templates, and tests

--- Frontend-Angular/src/app/core/layout/domain-searchbar/domain-searchbar.component.ts
+++
@@ -1,7 +0,0 @@
-﻿import { Component } from '@angular/core';
-@Component({
-  selector: 'app-domain-searchbar',
-  templateUrl: './domain-searchbar.component.html',
-})
-export class DomainSearchbarComponent {
-}
