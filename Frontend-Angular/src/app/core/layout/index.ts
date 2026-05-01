9ceb1a7fdee85ee682a7601a01ea97f99e25c362
RLNunes <38779@alunos.isel.ipl.pt>
2026-05-01 23:35:44 +0100

    :building_construction: feat: add index barrel files for core layout and public feature components

---
+++ Frontend-Angular/src/app/core/layout/index.ts
@@ -0,0 +1,6 @@
+export * from './shell/shell.component';
+export * from './user-menu/user-menu.component';
+export * from './global-header/global-header.component';
+export * from './admin-domain-shell/admin-domain-shell.component';
+export * from './public-domain-shell/public-domain-shell.component';
+export * from './consulta-domain-shell/consulta-domain-shell.component';
---
+++ Frontend-Angular/src/app/features/public/index.ts
@@ -0,0 +1,6 @@
+export * from './home/home.component';
+export * from './mandado/mandado.component';
+export * from './consulta/consulta.component';
+export * from './ultramar/ultramar.component';
+export * from './respostas/respostas.component';
+export * from './provocacao/provocacao.component';
