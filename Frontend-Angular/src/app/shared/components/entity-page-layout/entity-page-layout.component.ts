fb0876725ec48d6b0d7dd2d139d7b204819dc432
RLNunes <38779@alunos.isel.ipl.pt>
2026-05-01 22:18:40 +0100

    :recycle: refactor: update `EntityPageLayoutComponent` to use content projection and simplify inputs

--- Frontend-Angular/src/app/shared/components/entity-page-layout/entity-page-layout.component.ts
+++ Frontend-Angular/src/app/shared/components/entity-page-layout/entity-page-layout.component.ts
@@ -1,14 +1,9 @@
-﻿import {Component, Input} from '@angular/core';
-import {GenericFilterFormComponent} from '../generic-filter-form/generic-filter-form.component';
+﻿import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-entity-page-layout',
-  imports: [
-    GenericFilterFormComponent
-  ],
   templateUrl: './entity-page-layout.component.html',
 })
 export class EntityPageLayoutComponent {
-  @Input() title!: string;
 }
 
