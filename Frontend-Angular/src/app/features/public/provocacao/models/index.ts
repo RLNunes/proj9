2c408545cfa60338c963e761e91b829e1e4cc3a8
RLNunes <38779@alunos.isel.ipl.pt>
2026-05-02 23:34:49 +0100

    :building_construction: feat: add `ProvocacaoSearchFilters` model and export in index barrel files

--- Frontend-Angular/src/app/features/public/provocacao/models/index.ts
+++ Frontend-Angular/src/app/features/public/provocacao/models/index.ts
@@ -1,2 +1,3 @@
 export * from './provocacao.model';
 export * from './provocacao-api.model';
+export * from './provocacao-filter.model';
---
+++ Frontend-Angular/src/app/features/public/provocacao/models/provocacao-filter.model.ts
@@ -0,0 +1,6 @@
+export interface ProvocacaoSearchFilters {
+  search: string;
+  pessoa: string;
+  tema: number;
+  palavraChave: number;
+}
