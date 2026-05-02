25b81fdf07bdb43bc4cf77ebe0781439663b8e52
RLNunes <38779@alunos.isel.ipl.pt>
2026-05-02 14:30:06 +0100

    :building_construction: feat: export new shared components in index barrel file

--- Frontend-Angular/src/app/shared/components/index.ts
+++ Frontend-Angular/src/app/shared/components/index.ts
@@ -1,1 +1,3 @@
 export * from './page-title/page-title.component';
+export * from './entity-page-layout/entity-page-layout.component';
+export * from './generic-filter-form/generic-filter-form.component';
